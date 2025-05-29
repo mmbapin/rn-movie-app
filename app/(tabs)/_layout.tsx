d2437c16de85723e584c58127864fa9846ae3a0e
mmbapin <bapinmm@gmail.com>
2025-05-29 11:30:39 +0600

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,57 @@
+import { icons } from '@/constants/icons';
+import { images } from '@/constants/images';
+import { ImageBackground } from 'expo-image';
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { Image, Text } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const _layout = () => {
+	return (
+		<Tabs>
+			<Tabs.Screen
+				name='index'
+				options={{
+					title: 'Home',
+					headerShown: false,
+					tabBarIcon: ({ focused }) => (
+						<>
+							<ImageBackground
+								source={images.highlight}
+								className='flex flex-row w-full flex-1 min-w-[112w] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
+							>
+								<Image source={icons.home} tintColor='#151312' className='size-5' />
+								<Text className='text-secondary text-base font-semibold ml-2'>Home</Text>
+							</ImageBackground>
+						</>
+					),
+				}}
+			/>
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+			<Tabs.Screen
+				name='search'
+				options={{
+					title: 'Search',
+					headerShown: false,
+				}}
+			/>
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
-}
+			<Tabs.Screen
+				name='saved'
+				options={{
+					title: 'Saved',
+					headerShown: false,
+				}}
+			/>
+
+			<Tabs.Screen
+				name='profile'
+				options={{
+					title: 'Profile',
+					headerShown: false,
+				}}
+			/>
+		</Tabs>
+	);
+};
+
+export default _layout;
